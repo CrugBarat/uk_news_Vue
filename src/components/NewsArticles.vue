3dba0cefd4c3d0825f93d98d04607b10f2607141
Craig Barratt <craigtbarratt@gmail.com>
2020-04-24 22:00:41 +0100

    adds news articles styling

--- src/components/NewsArticles.vue
+++ src/components/NewsArticles.vue
@@ -2,7 +2,7 @@
   <div class="article">
     <a :href="article.url" target="_blank">
       <img class="article-image":src="article.urlToImage">
-      <p>{{article.title}}</p>
+      <p class="title">{{article.title}}</p>
       <p>{{article.description}}</p>
     </a>
   </div>
@@ -22,6 +22,8 @@ export default {
   color: black;
   flex: 1, 0, auto;
   width: 19%;
+  text-align: left;
+  margin: 4px;
 }
 
 a {
@@ -31,6 +33,14 @@ a {
 
 .article-image {
   width: 270px;
-  height: 270px;
+  height: 200px;
+}
+
+.title {
+  padding: 0;
+  margin: 0;
+  font-family: 'Open Sans';
+  font-size: 15px;
+  font-weight: 900;
 }
 </style>
